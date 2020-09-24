02d861cbe0ae58b93e32a9cf7b50460a9d0d659d
Jonathan Prust Vernizzi <jonathan@flipfloplab.com>
2020-09-24 16:23:46 -0300

    Changed function connectToWifi to use another command, because on raspbian, nmcli was not listing available wifi-networks as intended, hence not connecting too, because it only allows connections to networks it can see. I was receiving this error: No network with SSID X found. I was able to retrieve the list of available networks with iwlist and then creating a connection profile with nmcli and running it with 'nmcli con up profile_name'

--- src/linux-connect.js
+++ src/linux-connect.js
@@ -2,28 +2,51 @@ var execFile = require('child_process').execFile;
 var env = require('./env');
 
 function connectToWifi(config, ap, callback) {
-  var args = [];
-  args.push('-w');
-  args.push('10');
-  args.push('device');
-  args.push('wifi');
-  args.push('connect');
-  args.push(ap.ssid);
-  args.push('password');
-  args.push(ap.password);
 
-  if (config.iface) {
-    args.push('ifname');
-    args.push(config.iface);
-  }
+  // nmcli con add type wifi ifname wlan0 con-name SSID autoconnect yes ssid SSID
+  var args = ['con', 'add', 'type', 'wifi', 'ifname', 'wlan0', 'con-name', ap.ssid, 'autoconnect', 'yes', 'ssid', ap.ssid];
 
   execFile('nmcli', args, { env: env }, function(err, resp) {
     // Errors from nmcli came from stdout, we test presence of 'Error: ' string
     if (resp.includes('Error: ')) {
       err = new Error(resp.replace('Error: ', ''));
+      callback && callback(err);
+    } else {
+      // nmcli con modify SSID wifi-sec.key-mgmt wpa-psk
+      args = ['con', 'modify', ap.ssid, 'wifi-sec.key-mgmt', 'wpa-psk']
+      execFile('nmcli', args, { env: env }, function(err, resp) {
+
+        if (resp.includes('Error: ')) {
+          err = new Error(resp.replace('Error: ', ''));
+          callback && callback(err);
+        } else {
+          // nmcli con modify SSID wifi-sec.psk "PASSWORD"
+          args = ['con', 'modify', ap.ssid, 'wifi-sec.psk', ap.password]
+
+          execFile('nmcli', args, { env: env }, function(err, resp) {
+
+            if (resp.includes('Error: ')) {
+              err = new Error(resp.replace('Error: ', ''));
+              callback && callback(err);
+            } else {
+              // nmcli con up SSID
+              args = ['con', 'up', ap.ssid]
+
+              execFile('nmcli', args, { env: env }, function(err, resp) {
+
+                if (resp.includes('Error: ')) {
+                  err = new Error(resp.replace('Error: ', ''));
+                }
+                callback && callback(err);
+              });
+            }
+          });
+        }
+      });
     }
-    callback && callback(err);
+
   });
+
 }
 
 module.exports = function(config) {
